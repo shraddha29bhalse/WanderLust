ecb4c720813e133b4e03ac2ebb2047016a6c3edf
shraddha_Bhalse <bhalseshraddha@gmail.com>
2024-08-16 19:23:40 +0530

    root router issue3

--- app.js
+++ app.js
@@ -88,7 +88,7 @@ app.use((req,res,next)=>{
 
 app.use("/listings",listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
-app.use("/",listingRouter);
+app.use("/",userRouter);
 
 app.all("*", (req, res, next) => {
     next(new ExpressError(404, "Page Not Found!"));
